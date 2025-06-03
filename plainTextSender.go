e667b77e7174a009735efd4443d3df3e3ab36319
extraorange <106819626+extraorange@users.noreply.github.com>
2025-06-03 20:05:56 +0300

    plaintext module added

---
+++ plainTextSender.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+type PlainTextRequest struct {
+	Line string
+}
+
+func (r *PlainTextRequest) Serialize() ([]byte, error) {
+	return []byte(r.Line), nil
+}
+
+func (r *PlainTextRequest) Size() int {
+	return len(r.Line)
+}
+
+func NewPlainTextSender(opts LogSenderOpts) *GenericSender {
+	headers := make(map[string]string, len(opts.Headers)+1)
+	for k, v := range opts.Headers {
+		headers[k] = v
+	}
+	headers["Content-Type"] = "text/plain"
+	lines := opts.Lines
+	rnd := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	return &GenericSender{
+		LogSenderOpts: LogSenderOpts{
+			ID:         opts.ID,
+			Containers: opts.Containers,
+			Lines:      opts.Lines,
+			LinesPS:    opts.LinesPS,
+			URL:        opts.URL,
+			ReaderURL:  opts.ReaderURL,
+			Headers:    headers,
+		},
+		path:       "/test-lines",
+		timeout:    time.Second,
+		numOfSends: opts.LinesPS,
+		rnd:        rnd,
+		generate: func() IRequest {
+			line := lines[rnd.Intn(len(lines))]
+			return &PlainTextRequest{Line: line}
+		},
+	}
+}
